4b2eee8b7e95c568bdf30529dbd6fbd0659987a2
Julien Rechenmann <rechenmann@gmail.com>
2020-04-03 23:04:58 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,203 @@
+import os
+import time
+
+import pandas as pd
+from flask import Flask, session, render_template, redirect, url_for
+from flask_pymongo import PyMongo, request
+from cache.companies_cache import CompaniesCache
+from displayer.company_displayer import display_company
+from all_functions import print_companies_to_html
+from cryptography.fernet import Fernet
+from datetime import datetime
+from displayer.portfolio.portfolio_displayer import render_portfolio
+from cache.local_history_cache import LocalHistoryCache
+from cache.currencies import Currencies
+from displayer.portfolio.portfolio_displayer import format_amount
+from flask_simple_geoip import SimpleGeoIP
+import multiprocessing as mp
+
+app = Flask("Company Explorer")
+app.secret_key = os.environ["MONGO_KEY"]
+global pymongo_connected
+global companies_cache
+global mongo
+global tickers
+global history_cache
+global currencies
+
+pymongo_connected = False
+if 'MONGO_URI' in os.environ and not pymongo_connected:
+    app.config['MONGO_DBNAME'] = 'finance'
+    app.config['MONGO_URI'] = os.environ['MONGO_URI'].strip("'").replace('test', app.config['MONGO_DBNAME'])
+    mongo = PyMongo(app)
+    pymongo_connected = True
+    companies_cache = CompaniesCache(mongo.db.cleaned_companies)
+    history_cache = LocalHistoryCache(mongo.db.history)
+    tickers = pd.DataFrame.from_records(mongo.db.tickers.find())
+    currencies = Currencies()
+
+
+def is_user_connected():
+    if session.get("USER") and mongo.db.users.find_one({"email": session["USER"]}) is not None:
+        return True
+    elif session.get("USER"):
+        session.clear()
+    return False
+
+
+@app.route('/')
+def explore_companies():
+    if is_user_connected():
+        global pymongo_connected
+        html = "Database not connected"
+        current_country = request.args.get("country")
+        if current_country is None:
+            current_country = "All"
+        if pymongo_connected:
+            global companies_cache
+            df = companies_cache.copy()
+            df.reset_index(inplace=True)
+            df = pd.concat([df, pd.DataFrame(list(df["stats"]))], axis=1, sort=False)
+            if current_country != "All":
+                df = df[df.country == current_country]
+            df.set_index("ticker", inplace=True)
+            html = print_companies_to_html(df)
+        return render_template("companies.html", selected_country=current_country,
+                               countries=companies_cache.country.unique(),
+                               dividends=html)
+    else:
+        return redirect(url_for("login"))
+
+
+@app.route('/screener/<ticker>')
+def show_company(ticker):
+    global companies_cache
+    db_company = companies_cache.get(ticker)
+    if db_company is not None:
+        return display_company(db_company, ticker)
+    return "No company found"
+
+
+@app.route('/portfolios-manager', methods=['GET', 'POST'])
+def show_portfolio_manager():
+    if is_user_connected():
+        if request.method == 'POST':
+            data = request.form.to_dict(flat=True)
+            portfolio = mongo.db.portfolio.find_one({"email": session["USER"], "name": data["name"]})
+            if portfolio is None:
+                portfolio = {"email": session["USER"], "name": data["name"], "transactions": [], "total": 0,
+                             "currency": data["currency"], "invested": 0, "id_txn": 0}
+                mongo.db.portfolio.insert_one(portfolio)
+                return redirect(url_for("show_portfolio", name=data["name"]))
+            else:
+                return display_portfolios_manager()
+        else:
+            return display_portfolios_manager()
+    else:
+        return redirect(url_for("login"))
+
+
+def display_portfolios_manager():
+    global currencies
+    portfolios = list(mongo.db.portfolio.find({"email": session["USER"]}))
+    is_portfolio = len(portfolios)
+    for portfolio in portfolios:
+        portfolio["total"] = format_amount(portfolio["total"], portfolio["currency"])
+        if "current" in portfolio:
+            portfolio["current"] = format_amount(portfolio["current"], portfolio["currency"])
+        else:
+            portfolio["current"] = portfolio["total"]
+    keys = sorted(list(currencies.keys()))
+    return render_template("portfolios_manager.html", portfolios=portfolios,
+                           is_portfolio=is_portfolio,
+                           currencies=keys)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    global mongo
+    if session.get("USER"):
+        return redirect(url_for("show_portfolio"))
+    if request.method == 'GET':
+        return render_template("login.html")
+    if request.method == 'POST':
+        data = request.form
+        f = Fernet(bytes(os.environ["MONGO_KEY"], 'utf-8'))
+        users = list(mongo.db.users.find())
+        user = None
+        for tmp_user in users:
+            if isinstance(tmp_user["email"], bytes):
+                email = f.decrypt(tmp_user["email"]).decode("utf-8")
+                if email == data["email"]:
+                    user = tmp_user
+            elif tmp_user["email"] == data["email"]:
+                user = tmp_user
+                user["first_name"] = f.encrypt(user["first_name"].encode())
+                user["last_name"] = f.encrypt(user["last_name"].encode())
+                user["email"] = f.encrypt(user["email"].encode())
+                mongo.db.users.find_one_and_replace({"email": data["email"]}, user)
+                portfolios = list(mongo.db.portfolio.find({"email": data["email"]}))
+                for portfolio in portfolios:
+                    portfolio["email"] = user["email"]
+                    mongo.db.portfolio.find_one_and_replace({"email": data["email"]}, portfolio)
+        if user is not None:
+            db_pass = f.decrypt(user["pass"]).decode("utf-8")
+            if db_pass == data["pass"]:
+                session["KEY"] = os.environ["MONGO_KEY"]
+                session["USER"] = user["email"]
+                session.new = True
+                return redirect(url_for("show_portfolio"))
+            else:
+                return render_template("login.html", error_login="Wrong password")
+        else:
+            return render_template("login.html", error_login="No user found with username")
+
+
+@app.route('/logout')
+def logout():
+    if session.get("USER"):
+        data = mongo.db.users.find_one({"email": session["USER"]})
+        if data is not None:
+            data["connected"] = False
+            data["last_connection"] = datetime.now()
+        session.clear()
+    return redirect(url_for("login"))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    global mongo
+    if is_user_connected():
+        return redirect(url_for("show_portfolio"))
+    if request.method == 'GET':
+        return render_template("register.html")
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
+        f = Fernet(bytes(os.environ["MONGO_KEY"], 'utf-8'))
+        data["pass"] = f.encrypt(data["pass"].encode())
+        data["first_name"] = f.encrypt(data["first_name"].encode())
+        data["last_name"] = f.encrypt(data["last_name"].encode())
+        data["email"] = f.encrypt(data["email"].encode())
+        data["creation_date"] = datetime.now()
+        data["geolocation"] = simple_geoip.get_geoip_data()
+        data.pop("pass2")
+        user = None
+        try:
+            user = mongo.db.users.find_one({"email": data["email"]})
+        except:
+            pass
+        if user is None:
+            data["connected"] = True
+            data["last_connection"] = datetime.now()
+            mongo.db.users.insert_one(data)
+            session["KEY"] = os.environ["MONGO_KEY"]
+            session["USER"] = data["email"]
+            session.new = True
+            return redirect(url_for("show_portfolio"))
+        else:
+            return render_template("login.html", error_login="Email already in database. Try signing in")
+
+
+if __name__ == '__main__':
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.run()
---
+++ static/js/main.js
@@ -0,0 +1,183 @@
+function showCompany(ticker) {
+    window.location.href = "/screener/" + ticker;
+}
+
+function show_portfolio(portfolio_name) {
+    window.location.href = "/portfolio?name=" + portfolio_name;
+}
+
+function got_to_portfolio_manager() {
+    window.location.href = "/portfolios-manager";
+}
+
+function got_to_screener() {
+    window.location.href = "/";
+}
+
+
+function filterCountry() {
+    var e = document.getElementById("country");
+    var country = e.options[e.selectedIndex].value;
+    window.location.href = "/?country=" + country;
+}
+
+function computeTotal(currency) {
+    var total = document.getElementById("total_add");
+    var total_text = document.getElementById("total_add_text");
+    var shares = document.getElementById("shares").value;
+    var price = document.getElementById("price").value;
+    total.value = shares * price;
+    if (currency.length > 3) {
+        currency = "USD";
+    }
+    total_text.textContent = new Intl.NumberFormat('de-DE', {
+        style: 'currency',
+        currency: currency
+    }).format(shares * price);
+}
+
+function disconnect() {
+    window.location.href = "/logout";
+}
+
+function add_transaction(tickers) {
+    if (document.getElementById('add_transaction').reportValidity()) {
+        ticker = document.getElementById('ticker_input').value;
+        name = document.getElementById('name_input').value;
+        if (ticker in tickers && tickers[ticker] == name) {
+            document.getElementById('submit_transaction').disabled = true;
+            document.getElementById('submit_transaction').style.backgroundColor = "grey";
+            document.getElementById('add_transaction').submit();
+        } else {
+            error_div = document.getElementById("error_add_transaction");
+            error_div.style.display = "block";
+            error_div.innerHTML = "Error: Ticker and Name do not match.";
+        }
+    }
+}
+
+function add_portfolio(currencies) {
+    if (document.getElementById('add_portfolio_form').reportValidity()) {
+        currency = document.getElementById('portfolio_currency').value;
+        if (currencies.includes(currency)) {
+            document.getElementById('submit_portfolio').disabled = true;
+            document.getElementById('submit_portfolio').style.backgroundColor = "grey";
+            document.getElementById('add_portfolio_form').submit();
+        } else {
+            error_div = document.getElementById("error_add_transaction");
+            error_div.style.display = "block";
+            error_div.innerHTML = "Error: Currency " + currency + " does not exists.";
+        }
+    }
+}
+
+function fill_name(tickers) {
+    ticker = document.getElementById('ticker_input').value;
+    document.getElementById('name_input').value = tickers[ticker];
+}
+
+function fill_ticker(tickers) {
+    name = document.getElementById('name_input').value;
+    document.getElementById('ticker_input').value = getKeyByValue(tickers, name);
+}
+
+function go_to_portfolio_form() {
+    var elmnt = document.getElementById("add_portfolio_form");
+    elmnt.style.display = "block";
+    elmnt.scrollIntoView();
+}
+
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+function sortTable(n, element, ignore=1) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById(element);
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (i = ignore; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare,
+            one from current row and one from the next: */
+            x = rows[i].getElementsByTagName("TD")[n].innerHTML.toLowerCase();
+            x_float = parseFloat(x.replace(/[^\d.-]/g, ''));
+            if(!Number.isNaN(x_float)){
+                x = x_float;
+            }
+            y = rows[i + 1].getElementsByTagName("TD")[n].innerHTML.toLowerCase();
+            y_float = parseFloat(y.replace(/[^\d.-]/g, ''));
+            if(!Number.isNaN(x_float)){
+                y = y_float;
+            }
+            /* Check if the two rows should switch place,
+            based on the direction, asc or desc: */
+            if (dir == "asc") {
+                if (x > y) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x < y) {
+                    // If so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            // Each time a switch is done, increase this count by 1:
+            switchcount++;
+        } else {
+            /* If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again. */
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}
+
+Date.prototype.toDateInputValue = (function () {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0, 10);
+});
